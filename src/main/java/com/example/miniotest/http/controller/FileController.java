7ebb4d6b9b5dc52bb01f39620271c11faf0b5452
mohammadolama <rastegar123456789@gmail.com>
2023-01-18 12:18:57 +0330

    Initial commit

---
+++ src/main/java/com/example/miniotest/http/controller/FileController.java
@@ -0,0 +1,55 @@
+package com.piinalpin.minio.http.controller;
+
+import com.piinalpin.minio.http.dto.FileDto;
+import com.piinalpin.minio.service.MinioService;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.IOUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.AntPathMatcher;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.nio.file.Files;
+
+import static org.springframework.web.servlet.HandlerMapping.BEST_MATCHING_PATTERN_ATTRIBUTE;
+
+@Slf4j
+@RestController
+@RequestMapping(value = "/file")
+public class FileController {
+
+    private final MinioService minioService;
+
+    public FileController(MinioService minioService) {
+        this.minioService = minioService;
+    }
+
+    @GetMapping
+    public ResponseEntity<Object> getFiles() {
+        return ResponseEntity.ok(minioService.getListObjects());
+    }
+
+    @GetMapping(value = "/**")
+    public ResponseEntity<Object> getFile(HttpServletRequest request) throws IOException {
+        String pattern = (String) request.getAttribute(BEST_MATCHING_PATTERN_ATTRIBUTE);
+        String filename = new AntPathMatcher().extractPathWithinPattern(pattern, request.getServletPath());
+        return ResponseEntity.ok()
+                .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                .body(IOUtils.toByteArray(minioService.getObject(filename)));
+
+    }
+
+    @PostMapping(value = "/upload")
+    public ResponseEntity<Object> upload(@ModelAttribute FileDto request) {
+        return ResponseEntity.ok().body(minioService.uploadFile(request));
+    }
+
+    @PostMapping(value = "/folder/{name}")
+    public void createFolder(@PathVariable String name){
+        minioService.createFolder(name);
+    }
+
+}
---
+++ src/main/java/com/example/miniotest/swagger/SpringFoxConfig.java
@@ -0,0 +1,62 @@
+package com.example.utils.swagger;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+@Configuration
+@EnableSwagger2
+public class SpringFoxConfig {
+
+    public static final String AUTHORIZATION_HEADER = "Authorization";
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder().title("SalamAop").description("Salam Aop api").build();
+    }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .securityContexts(Collections.singletonList(securityContext()))
+                .securitySchemes(List.of(apiKey()))
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.example.controller"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(new ApiInfoBuilder().title("SalamProxy").description("Salam Proxy api").build());
+    }
+
+    private ApiKey apiKey() {
+        return new ApiKey("JWT", AUTHORIZATION_HEADER, "header");
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder()
+                .securityReferences(defaultAuth())
+                .build();
+    }
+
+    List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope
+                = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return List.of(new SecurityReference("JWT", authorizationScopes));
+    }
+
+}
